459ce81b8d12e55786d038392227e0d29a04c0d4
Catalin-Zaharia <catalinzaharia0112@gmail.com>
2023-04-08 18:34:24 +0300

    comments

--- breached.py
+++ breached.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, render_template_string, request, url_for, redirect, jsonify, make_response, flash, send_from_directory, send_file
+from flask import Flask, render_template, render_template_string, request, url_for, redirect, jsonify, make_response, send_from_directory, send_file
 from pymongo import MongoClient
 import hashlib
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, set_access_cookies, unset_jwt_cookies
@@ -10,7 +10,8 @@ import secrets
 import os
 
 PROFILE_PICTURES = os.path.join('static', 'profile_pictures')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg'}
 
 app = Flask(__name__)
 
@@ -21,11 +22,17 @@ db = client.flask_db
 
 users_collection = db.users
 posts_collection = db.posts
+comments_collection = db.comms
 
 try:
     posts_increment = posts_collection.find_one(sort=[("id", -1)])["id"]
 except:
     posts_increment = 0
+
+try:
+    comm_increment = comments_collection.find_one(sort=[("id", -1)])["id"]
+except:
+    comm_increment = 0
 
 jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'secret' #secrets.token_hex(16)
@@ -45,6 +52,13 @@ post_schema = {
     'title': str,
     'content': str,
     'user': str
+}
+
+comment_schema = {
+    'id': int,
+    'post_id':int,
+    'user': str,
+    'content': str,
 }
 
 @app.route('/register', methods=["GET", "POST"])
@@ -102,9 +116,43 @@ def get_template():
         owner = False
         if post["user"] == current_user:
             owner = True
-        return render_template('/post/get.html', post=post, owner=owner)
+        comments = comments_collection.find({"post_id":int(post_id)})
+        return render_template('/post/get.html', post=post, owner=owner, comments=comments)
 
     return redirect("/posts")
+
+@app.route("/postcomment", methods=["POST"])
+@jwt_required()
+def post_comment():
+    global comm_increment
+    if request.method == 'POST':
+        current_user = get_jwt_identity()
+        if current_user:
+            content = request.form['content']
+            post_id = request.form['post_id']
+            comm_increment += 1
+            comments_collection.insert_one({'id':comm_increment, 'user': current_user, 'content': content, 'post_id': int(post_id)})
+            response = redirect("/post?id="+str(post_id))
+        else:
+            response = redirect("/login")
+    else:
+        response = redirect("/")
+    
+    return response
+
+@app.route("/deletecomment", methods=["GET"])
+@jwt_required()
+def delete_comment():
+    current_user = get_jwt_identity()
+    comment_id = request.args.get("id")
+    if current_user:
+        try:
+            comment = comments_collection.find_one({"id":int(comment_id)})
+            comments_collection.delete_one(comment)
+        except:
+            return render_template_string("Error while trying to delete " + comment_id + "!")
+            
+    return render_template_string("Deleted " + comment_id + "!")
 
 
 @app.route("/post/create", methods=["GET", "POST"])
@@ -135,7 +183,9 @@ def delete_template():
     post_id = request.args.get("id")
     if current_user:
         try:
+            comments = comments_collection.find({"post_id":int(post_id)})
             post = posts_collection.find_one({"id":int(post_id)})
+            comments_collection.delete_many(comments)
             posts_collection.delete_one(post)
         except:
             return render_template_string("Error while trying to delete " + post_id + "!")
@@ -156,11 +206,9 @@ def my_profile():
     if request.method== "POST":
 
         if 'file' not in request.files:
-            flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
-            flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             file.save(os.path.join(app.config['PROFILE_PICTURES'], current_user+'.jpg'))
@@ -175,12 +223,17 @@ def my_profile():
 @app.route("/photos", methods=["GET"])
 def get_photo():
     path = request.args.get("photo")
-    return send_file(app.config["PROFILE_PICTURES"] +'/' +path)
+    try:
+        return send_file(app.config["PROFILE_PICTURES"] +'/' +path)
+    except:
+        return make_response("<pre>THIS\n"+"IS\n"+"WHERE\n"+"YOUR\n"+"PROFILE\n"+"PICTURE\n"+"WOULD\n"+"BE\n"+"IF\n"+"YOU\n"+"HAD\n"+"ONE\n"+"</pre>")
 
 app.add_url_rule(
     "/photos", endpoint="get_photo", build_only=True
 )
 
+
+@app.route("/")
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
